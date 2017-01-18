0e5b93a44c0c9d4ec233cc59b297a8e1b4f765f1
Anders Dalskov <anderspkd@gmail.com>
2017-01-18 13:12:15 +0100

    Whoops. Stupid mistake.

--- generate-test-values.php
+++ generate-test-values.php
@@ -8,8 +8,8 @@ if(count($argv) == 2) {
 
 for($x = 0; $x < $until; $x++) {
     $uid = uniqid('', true);
-    /* printf("%s %d\n", $uid, (1/4.656613e-10)*((float)substr($uid, -10) / 10));*/
-    printf("%s\n", $uid);
+    printf("%s %d\n", $uid, (1/4.656613e-10)*((float)substr($uid, -10) / 10));
+    /* printf("%s\n", $uid);*/
 }
 
 ?>
